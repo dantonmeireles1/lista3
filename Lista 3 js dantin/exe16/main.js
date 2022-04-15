d012c67e2edd419cee6a0d418a8e766822f6e8a2
DantonMeireles1 <dantongit1@gmail.com>
2022-04-14 22:54:51 -0300

    16 ta pronta

--- Lista 3 js dantin/exe16/main.js
+++ Lista 3 js dantin/exe16/main.js
@@ -1,4 +1,4 @@
-//DEFININDO VARIAVEL
+/*//DEFININDO VARIAVEL
 var anacl = 1.50, all = 0.02, felis = 1.10, all2 = 0.03, ano = 1;
 
 //FAÇA ENQUANTO
@@ -8,4 +8,16 @@ do{
 	ano++;
 }while(felis<anacl);
 alert("Serão necessários "+ano+" anos");
-console.aler("Serão necessários "+ano+" anos");
+console.aler("Serão necessários "+ano+" anos");*/
+
+var anacl = 1.50, all = 0.02, felis = 1.10, all2 = 0.03, ano = 1;
+function altura(anacl, all, felis, all2, ano){
+	do{
+		anacl = anacl + all;
+		felis = felis + all2;
+		ano++;
+	}while(felis<anacl);
+	return ano;
+}
+ano = altura(anacl, all, felis, all2, ano);
+console.log("Serão necessários: "+ano+" anos");
--- Lista 3 js dantin/exe9/main.js
+++ Lista 3 js dantin/exe9/main.js
@@ -22,5 +22,34 @@ for(i=1; i<=4; i++){
 //EXIBINDO
 alert("Maior: "+maior);
 alert("Menor: "+menor);
-console.alert("Maior: "+maior);
-console.alert("Menor: "+menor);*/
+console.log("Maior: "+maior);
+console.log("Menor: "+menor);*/
+
+var maior,menor,numero,a=0,b=0,c=0,d=0;
+function maior1(a,b){
+    b = a;
+    if(b>a){
+        a = b;
+    }return a;
+}
+
+function menor1(c,d){
+    d = c;
+    if(d<c){
+        c = d;
+    }return c;
+}
+
+numero = parseInt(prompt("Entre com um número inteiro: "));
+maior = numero;
+menor = numero;
+for(i=1; i<=4; i++){
+    numero = parseInt(prompt("Digite um número: "));
+    if(numero<menor){
+        menor = menor1(c,d);
+    }else if(numero>maior){
+        maior = maior1(a,b);
+    }
+}
+console.log("Maior: "+maior);
+console.log("Menor: "+menor);
